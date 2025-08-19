c1c8d8fd09db6d14f087bd41044d1e0ab4beef09
AgustinRiple <agusriple@gmail.com>
2025-08-19 00:08:49 -0300

    flaskterceravez

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
 def hello():
-    return "¡Hola, mundo!"
-
-if name == "main":
-    app.run(debug=True)
+ return "¡Hola, mundo!"
+if __name__ == "__main__":
+ app.run(debug=True)
